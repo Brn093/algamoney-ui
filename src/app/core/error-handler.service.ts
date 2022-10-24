982bd636cbd82dbf79eef2f81197d92522570e33
Joao Bruno Freitas Vasconcelos <joaobruno.freitas@uece.br>
2022-10-24 16:57:40 -0300

    E se o Refresh Token expirar?

--- src/app/core/error-handler.service.ts
+++ src/app/core/error-handler.service.ts
@@ -1,18 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
+import { NotAuthenticatedError } from '../seguranca/money-http-interceptor';
 @Injectable({
   providedIn: 'root'
 })
 export class ErrorHandlerService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private messageService: MessageService,
+    private router: Router
+    ) { }
 
   handle(errorResponse: any) {
     let msg!: string;
 
     if (typeof errorResponse === 'string') {
       msg = errorResponse;
-    } else if (errorResponse instanceof Response && errorResponse.status >= 400 && errorResponse.status <= 499) {
+    } else if (errorResponse instanceof NotAuthenticatedError) {
+      msg = 'Sua sessão expirou!';
+      this.router.navigate(['/login']);
+
+    } 
+    else if (errorResponse instanceof Response && errorResponse.status >= 400 && errorResponse.status <= 499) {
       let errors;
       msg = 'Ocorreu um erro ao processar a sua solicitação';
 
