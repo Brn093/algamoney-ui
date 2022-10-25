27039b50c2a735b957f0b1cb7c489e9026e5630c
Joao Bruno Freitas Vasconcelos <joaobruno.freitas@uece.br>
2022-10-25 20:00:18 -0300

    Respondendo requisições com Node.js e Express

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const projectName = 'algamoney-ui'
+
+app.use(express.static(__dirname + '/dist/' + 'meuprojeto'));
+
+app.get('/*', function (req, res) {
+  res.sendFile(__dirname + '/dist/' + 'meuprojeto' + '/index.html');
+});
+
+app.listen(4200);
