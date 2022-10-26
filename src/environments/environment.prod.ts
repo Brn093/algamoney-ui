5c37b9cfbb785adfd5b1d1a7b00f432cf0d89f8f
Joao Bruno Freitas Vasconcelos <joaobruno.freitas@uece.br>
2022-10-26 00:04:37 -0300

    Novo Deploy com o enviroment modificado

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://algaworks-money-api.herokuapp.com'
+  apiUrl: 'https://algaworks-money-api.herokuapp.com',
+  tokenAllowedDomains: [ /algaworks-money-api.herokuapp.com/ ],
+  tokenDisallowedRoutes: [/\/oauth\/token/],
 };
