1058a261ec74945d26b7ee00a0c56da594db0255
Joao Bruno Freitas Vasconcelos <joaobruno.freitas@uece.br>
2022-11-29 15:07:05 -0300

    Criando o módulo Dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { NaoAutorizadoComponent } from './core/nao-autorizado.component';
 
 import { PaginaNaoEncontradaComponent } from './core/pagina-nao-encontrada.component';
+import { DashboardModule } from './dashboard/dashboard.module';
 import { PessoasPesquisaComponent } from './pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
 
 const routes: Routes = [
   { path: 'lancamentos', loadChildren: () => import('../app/lancamentos/lancamentos.module').then(m => m.LancamentosModule) },
   { path: 'pessoas', loadChildren: () => import('../app/pessoas/pessoas.module').then(m => m.PessoasModule) },
-  { path: '', redirectTo: 'lancamentos', pathMatch: 'full' },  
+  { path: 'dashboard', loadChildren: () => import('../app/dashboard/dashboard.module').then(m => m.DashboardModule) },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },  
   //{ path: 'pessoas', component: PessoasPesquisaComponent },
   { path: 'nao-autorizado', component: NaoAutorizadoComponent },
   { path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent },
